6b81caed8d5020ce9bd638eb808cede8296913d1
lautarogerlero <93661302+lautarogerlero@users.noreply.github.com>
2021-12-25 17:49:01 -0300

    Version 1.0

---
+++ resultados.py
@@ -0,0 +1,12 @@
+# Funcion para editar el archivo "ganadores"
+
+import csv
+
+def agregar_ganadores(ganador, puntaje):
+    header = ["ganador", "puntaje total"]
+    csvfile = open("ganadores.csv", "a")
+    writer = csv.DictWriter(csvfile, fieldnames=header)
+    nuevo_ganador = {"ganador": ganador, "puntaje total":puntaje}
+    writer.writerow(nuevo_ganador)
+    csvfile.close()
+
