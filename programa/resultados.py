2bb555b097a584cd06e22c3735776bf9c0c3903a
lautarogerlero <93661302+lautarogerlero@users.noreply.github.com>
2021-12-27 18:53:02 -0300

    Solucionado el problema en "Resultados"

--- programa/resultados.py
+++ programa/resultados.py
@@ -4,7 +4,7 @@ import csv
 
 def agregar_ganadores(ganador, puntaje):
     header = ["ganador", "puntaje total"]
-    csvfile = open("ganadores.csv", "a")
+    csvfile = open("ganadores.csv", "a", newline="")
     writer = csv.DictWriter(csvfile, fieldnames=header)
     nuevo_ganador = {"ganador": ganador, "puntaje total":puntaje}
     writer.writerow(nuevo_ganador)
